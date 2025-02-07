b0e3e33597a25a40e52dce70f67214957e647078
nikgraz <graziano.nicholas@itisgrassi.edu.it>
2025-02-07 12:29:42 +0100

    Add files via upload

---
+++ 05_concatenazione.py
@@ -0,0 +1,6 @@
+n1 = int(input("scrivi un numero: "))
+n2 = int(input("scrivi un altro numero: "))
+risultato1 = n1+n2
+print ("il primo risultato è:", risultato1)
+risultato2 = n1*3
+print ("il secondo risultato è:", risultato2)
---
+++ 06_concatenazione.py
@@ -0,0 +1,17 @@
+a = int(input("scrivi il numero; "))
+b = int(input("scrivi un secondo numero: "))
+c = a + b
+d = a - b 
+e = a * b
+f = a / b
+g = a // b
+h = a % b 
+i = a ** b 
+print("il risultato delle operazioni è: ")
+print("a + b = ",c)
+print("a - b = ",d)
+print("a * b =", e)
+print("a / b = ",f)
+print(" a // b",g)
+print("a % b =",h)
+print("a ** b = ",i)
